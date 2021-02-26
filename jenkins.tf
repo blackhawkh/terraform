49d8f488dccbcdebc7bf59e8f4141938d968d563
Andrii Zakhariashchev <a.zakhariashchev@spd-ukraine.com>
2021-02-26 13:27:04 +0200

    add notes

--- jenkins.tf
+++ jenkins.tf
@@ -1,6 +1,7 @@
 // Can be create static public IP and configure DNS in any name server (Google DNS, CloudFlare etc)
 // For normal production need to move docker registry to separate server(-s) or SaaS solution
 // need to create additional disk for $JENKINS_HOME (it can be resized on the fly and have possibility to backup)
+// via metadata can be provided initial jenkins password, or just connect Jenkins to LDAP/SSO
 
 resource "google_compute_instance" "jenkins" {
   machine_type = "n1-standard-2"
